0e92b29caaf87413c80499031328d8a6acd621ba
shapl_xlbobtq <sergeshapluk@gmail.com>
2024-12-10 20:19:11 +0300

    fix

--- src/components/ui/modal/ModalError.tsx
+++ src/components/ui/modal/ModalError.tsx
@@ -1,44 +1,42 @@
-// import ReactModal from "react-modal";
-// import MemoAlertIcon from "../../svg/AlertIcon";
-//
-//
-// // ReactModal.setAppElement("#root");
-//
-// type ModalErrorProps = {
-//     error: { isOpen: boolean, message: string }
-//     close: () => void
-// }
-//
-// function ModalError({error, close}: ModalErrorProps) {
-//     return (
-//         <>
-//             <ReactModal isOpen={error.isOpen} onRequestClose={close} style={{
-//                 overlay: {background: "rgba(0,0,0,0.7)", zIndex: 999}, content: {
-//                     width: "95%",
-//                     top: 0,
-//                     left: "50%",
-//                     right: "auto",
-//                     bottom: "auto",
-//                     marginRight: "-50%",
-//                     transform: "translate(-50%, 10px)",
-//                     border: "none",
-//                     borderRadius: 12,
-//                     background: "#272727"
-//                 }
-//             }}>
-//                 <div style={{
-//                     display: "flex",
-//                     flexDirection: "row",
-//                     alignItems: "center"
-//                 }}>
-//                     <div>
-//                         <MemoAlertIcon/>
-//                     </div>
-//                     <div style={{marginLeft: 12}}>{error.message}</div>
-//                 </div>
-//             </ReactModal>
-//         </>
-//     );
-// }
-//
-// export default ModalError;
+import Modal from "react-modal";
+import MemoAlertIcon from "../../svg/AlertIcon";
+
+
+Modal.setAppElement("#root");
+
+type ModalProps = {
+    error: { isOpen: boolean, message: string }
+    close: () => void
+}
+
+function ModalError({error, close}: ModalProps) {
+    return (
+        <Modal isOpen={error.isOpen} onRequestClose={close} style={{
+            overlay: {background: "rgba(0,0,0,0.7)", zIndex: 999}, content: {
+                width: "95%",
+                top: 0,
+                left: "50%",
+                right: "auto",
+                bottom: "auto",
+                marginRight: "-50%",
+                transform: "translate(-50%, 10px)",
+                border: "none",
+                borderRadius: 12,
+                background: "#272727"
+            }
+        }}>
+            <div style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center"
+            }}>
+                <div>
+                    <MemoAlertIcon/>
+                </div>
+                <div style={{marginLeft: 12}}>{error.message}</div>
+            </div>
+        </Modal>
+    );
+}
+
+export default ModalError;
